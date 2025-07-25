7fa8bf601a79fd27a9e0f89a790641f38c232aed
hashhere1 <aleemhassaan70@gmail.com>
2025-07-25 19:30:13 +0500

    Commititng test cases

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,16 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import Base
+from app.main import app
+from app.database import get_db
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///:memory:")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-connection = engine.connect()
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=connection)
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
-Base.metadata.create_all(bind=connection)
+Base.metadata.create_all(bind=engine)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def override_get_db():
     db = TestingSessionLocal()
@@ -18,3 +21,6 @@ def override_get_db():
         yield db
     finally:
         db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
